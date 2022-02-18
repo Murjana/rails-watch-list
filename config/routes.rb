d1fd79a19363f833baaa788144996a224363da77
murjana younis <murjana.younis@gmail.com>
2022-02-18 15:37:06 +0100

    home is list index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root to: "lists#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [:new, :show, :index, :create] do
+  resources :lists, only: [:new, :show, :create] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
